0b6b50fdab78687f67c41d73a0b2eed6b576713e
Mikael Schelin <mrsc@kth.se>
2022-02-11 14:18:04 +0100

    object version

--- script.js
+++ script.js
@@ -1,5 +1,11 @@
 // this is a random elevator pitch generator
 
+const PitchObj = {
+    problem: [],
+    buzz: [],
+    solution: []
+}
+
 let problemArr = ['your happiness','the world hunger', 'covid pandemic', 'the housing problem', 'multi planetary problem', 'world poverty', 'your sex life', 'your dishes', 'fashion', 'metaverse'];
 let buzzArr = ['disruptive', 'revolutionary', 'super', 'awesome', 'giga', 'AI', 'intelligent', 'lean', 'fast', 'machine learning', 'algorithm', 'algoritmic', 'scientific', 'design', 'synthetic', 'evolutionary', 'quantum'];
 let solutionArr = ['social media', 'mobile application', 'robot', 'flying cars', 'pills', 'machines', 'yoga', 'voyage', 'wallet', 'trans', 'giga factory'];
